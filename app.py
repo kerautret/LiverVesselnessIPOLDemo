290976e8e4f27975fda54ce173fb43384d5fa416
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2020-04-09 01:40:08 +0200

    test

--- app.py
+++ app.py
@@ -269,8 +269,8 @@ class app(base_app):
         ##  -------
         ## process 3: convert off to obj
         ## ---------
-        f = open(self.work_dir+"output.txt", "w")
-        fInfo = open(self.work_dir+"info.txt", "w")
+        f = open(self.work_dir+"outputConvert.txt", "w")
+        fInfo = open(self.work_dir+"infoConvert.txt", "w")
         command_args = ['off2obj', '-i' , 'res.off', '-o', 'res.obj', '-c', '-n' ]        
         p = self.run_proc(command_args, stderr=fInfo, env={'LD_LIBRARY_PATH' : self.bin_dir})
         self.wait_proc(p, timeout=120)
