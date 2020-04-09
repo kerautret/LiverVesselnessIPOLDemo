c01294b273b0637348c1c7600645152d31fcce2b
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2020-04-09 23:16:07 +0200

    test name

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ class app(base_app):
         f = open(self.work_dir+"output.txt", "w")
         f.write("test write output...")
         fInfo = open(self.work_dir+"info.txt", "w")
-        command_args = ['Antiga', '-i' , 'inputVol_0.mha', '-o', 'res.nii', \
+        command_args = ['Antiga', '-i' , 'self.input_dir +self.cfg['meta']['basename']', '-o', 'res.nii', \
                         '-m', str(float(self.cfg['param']['sigmamin'])),
                         '-M', str(float(self.cfg['param']['sigmamax'])),
                         '-s', str(int(self.cfg['param']['steps'])),
